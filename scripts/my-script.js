502afa4d6d276b26115690328f9d839e0b336577
Alexander <alex@alexvoiceover.com>
2023-07-15 17:53:48 +0100

    The text will show when the image is showed half

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,16 +1,23 @@
-window.addEventListener('scroll', function() {
-  document.querySelectorAll('section').forEach(section => {
-      const img = section.querySelector('img');
-      const title = section.querySelector('.info h2');
-      const description = section.querySelector('.info p');
-      let rect = img.getBoundingClientRect();
-      
-      if(rect.left >= 0 && rect.left <= window.innerWidth) {
-          title.style.transform = 'translateY(0)';
-          description.style.transform = 'translateY(0)';
-      } else {
-          title.style.transform = 'translateY(100%)';
-          description.style.transform = 'translateY(-100%)';
-      }
-  });
+var container = document.getElementById('main-container');
+container.addEventListener('scroll', function() {
+    var scrollLeft = container.scrollLeft;
+    document.querySelectorAll('section').forEach(function(section, i) {
+        var img = section.querySelector('img');
+        var title = section.querySelector('h2');
+        var text = section.querySelector('p');
+
+        // calculate the start and end boundaries of each section
+        var sectionStart = img.width * i + img.width / 2;
+        var sectionEnd = img.width * (i + 1);
+
+        if (scrollLeft >= sectionStart && scrollLeft < sectionEnd) {
+            // If the section is in the viewport, animate the title and text
+            title.style.bottom = '50%';
+            text.style.top = '50%';
+        } else {
+            // If the section is not in the viewport, reset the title and text position
+            title.style.bottom = '100%';
+            text.style.top = '100%';
+        }
+    });
 });
